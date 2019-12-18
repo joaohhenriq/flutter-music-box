0eaec3dae615f4dc167cfa0a60377a2c19eb3779
joaoh <joao_camargo10@hotmail.com>
2019-12-18 20:43:13 -0300

    First commit

---
+++ lib/grid_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GridWidget extends StatelessWidget {
+
+  final int gridSize;
+
+  const GridWidget({Key key, this.gridSize}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
